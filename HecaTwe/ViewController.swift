ef9be5da056e4217e1c118681009985f7ca38ea3
Yamagn <syanekenpon@gmail.com>
2018-11-12 20:56:16 +0900

    Initial Commit

---
+++ HecaTwe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HecaTwe
+//
+//  Created by ymgn on 2018/11/12.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
